55b158271081037ff32176ff4009dc05f56a411c
elburhan <burhanbobe@icloud.com>
2024-04-24 20:50:17 +0000

    update

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,12 +1,11 @@
-import { uploadPhoto, createUser } from "./utils.js";
+import { uploadPhoto, createUser } from './utils';
 
-async function handleProfileSignup() {
-    try {
-        const [photo, user] = await Promise.all([uploadPhoto(), createUser()]);
-        console.log(`${user.firstName} ${user.lastName}`);
-    } catch (error) {
-        console.error("Signup system offline");
-    }
+export default function handleProfileSignup() {
+  const p1 = uploadPhoto();
+  const p2 = createUser();
+
+  return Promise.all([p1, p2]).then((value) => {
+    console.log(`${value[0].body} ${value[1].firstName} ${value[1].lastName}`);
+  })
+    .catch(() => { console.log('Signup system offline'); });
 }
-
-export default handleProfileSignup;
